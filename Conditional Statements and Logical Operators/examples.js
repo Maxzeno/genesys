474f1566c5c9b4da6b5c1d603ab33680f3947cae
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-05-19 19:13:45 +0100

    added examples.js file with some js syntax examples

---
+++ Conditional Statements and Logical Operators/examples.js
@@ -0,0 +1,61 @@
+// Syntax for if statement
+if (condition) {
+  //  block of code to be executed if condition is true
+}
+
+
+
+// Syntax for if and else statement
+if (condition) {
+  //  block of code to be executed if condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
+
+
+
+// Syntax for if, else if and else statement
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
+
+
+
+// Syntax for switch case statement without default
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+}
+
+
+
+// Syntax for switch case statement with default
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+
+
+
+// Syntax for simple if statement
+// if (condition)
+// 	console.log('day is five')
+
+
+
+// Syntax Conditional (Ternary) Operator
+// variablename = (condition) ? if_condition_is_true:if_condition_is_false
