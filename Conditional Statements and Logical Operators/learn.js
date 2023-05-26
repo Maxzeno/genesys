8ffb6c9ed1bc1eee9fe6bc4f26d18e65fcb53568
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-05-26 21:58:18 +0100

    Conditional Statements and Logical Operators

---
+++ Conditional Statements and Logical Operators/learn.js
@@ -0,0 +1,105 @@
+// if statement
+
+// if (5 > 200){
+//  	console.log("five is greater")
+// }
+
+
+
+// if else statement
+
+// age = 10
+// year = 2023
+
+// if (age > year){
+// 	console.log("age is greater")
+// } else {
+// 	console.log("year is greater")
+// }
+
+
+
+// if else and else if
+
+// if (1 == 10){
+// 	console.log("1 == 10")
+// } else if (5 == 50){
+// 	console.log("5 == 50")
+// } else if (6 > 200){
+// 	console.log("6 > 200")
+// } else {
+// 	console.log("None is true")
+// }
+
+
+
+// embed if statement
+
+// if (1 == 1){
+// 	console.log("1 == 1")
+// 	if (2 == 2){
+// 		console.log("1 == 1 and 2 == 2")
+// 	}
+// }
+
+
+// switch statement
+
+// days_of_the_week = "tuesday"
+
+// switch(days_of_the_week){
+// 	case "monday":
+// 	console.log("I love monday")
+// 	break;
+
+// 	case "sunday":
+// 	console.log("I go to church")
+// 	break
+
+// 	case "friday":
+// 	console.log("A happy day")
+// 	break
+
+// 	default:
+// 	console.log("None match")
+// }
+
+
+// if statement example for the above switch statement
+
+// days_of_the_week = "friday"
+
+// if (days_of_the_week == "monday"){
+// 	console.log("I love monday")
+// } else if (days_of_the_week == "sunday"){
+// 	console.log("I go to church")
+// } else if (days_of_the_week == "friday"){
+// 	console.log("A happy day")
+// } else {
+// 	console.log("None match")
+// }
+
+
+
+// check positive and negative numbers
+
+// number = 5
+
+// if (number >= 0){
+// 	console.log("positive number:", number)
+// } else {
+// 	console.log("negative number:", number)
+// }
+
+
+// if in if statement
+
+number = -6
+
+if (number >= 0){
+	if (number % 2 == 0){
+		console.log("We have a Even number")
+	} else {
+		console.log("We have an Odd number")
+	}
+}
